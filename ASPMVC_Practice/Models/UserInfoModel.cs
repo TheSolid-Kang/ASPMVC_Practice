6a918c7faf659165295eed3db45143bff0fc41b4
TheSolid-KANG <tortoise8947@gmail.com>
2023-08-22 18:52:07 +0900

    UserInfoModel 클래스 제작

---
+++ ASPMVC_Practice/Models/UserInfoModel.cs
@@ -0,0 +1,11 @@
+﻿namespace ASPMVC_Practice.Models
+{
+    public class UserInfoModel
+    {
+        public string? id_user { get; set; }
+        public string? id_name   {get;set;}
+        public string? id_qual   {get;set;}
+        public string? id_item   {get;set;}
+        public string? id_status {get;set;}
+    }
+}
