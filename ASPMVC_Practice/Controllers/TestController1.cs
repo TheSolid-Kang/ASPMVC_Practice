59f40ca06e619c1dfaa3e3e34ab1d715d3855693
TheSolid-KANG <tortoise8947@gmail.com>
2023-08-29 18:35:07 +0900

    컨트롤러 추가 성공: TestController1 추가로 사용

---
+++ ASPMVC_Practice/Controllers/TestController1.cs
@@ -0,0 +1,20 @@
+﻿using Engine._01.DBMgr;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.VisualStudio.Web.CodeGeneration.CommandLine;
+
+namespace ASPMVC_Practice.Controllers
+{
+    //[Area("Test1")]
+    public class TestController1 : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+        public IActionResult Test1()
+        {
+            var mgr = new MSSQL_Mgr();
+            return View();
+        }
+    }
+}
