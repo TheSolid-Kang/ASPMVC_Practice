b83019d899930f5ccbae02969207e5621cece6ad
TheSolid-KANG <tortoise8947@gmail.com>
2023-09-21 18:41:51 +0900

    entity framework 시도 2차: 성공 -- 성경 데이터 출력 됨

--- ASPMVC_Practice/Models/BibleModel.cs
+++ ASPMVC_Practice/Models/BibleModel.cs
@@ -1,7 +1,11 @@
-﻿namespace ASPMVC_Practice.Models
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace ASPMVC_Practice.Models
 {
     public class BibleModel
     {
+        [Key]
+        public int BibleSeq { get; set; }
         public string? Testament { get; set; }
         public int Chapter { get; set; }
         public int Verse { get; set; }
