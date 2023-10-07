1c34da79713d5d02ee4959f931ff003652c37872
TheSolid-KANG <tortoise8947@gmail.com>
2023-10-07 14:03:26 +0900

    _TCDiary 클래스 복사생성자 생성

--- ASPMVC_Practice/Models/_TCDiary.cs
+++ ASPMVC_Practice/Models/_TCDiary.cs
@@ -2,6 +2,18 @@
 {
   public class _TCDiary
   {
+        public _TCDiary() { }
+        public _TCDiary(_TCDiary _TCDiary) 
+        { 
+            ChurchSeq = _TCDiary.ChurchSeq;
+            DiarySeq = _TCDiary.DiarySeq;
+            InDate = _TCDiary.InDate;
+            Title = _TCDiary.Title;
+            Record = _TCDiary.Record;
+            Remark = _TCDiary.Remark;
+            LastUserSeq = _TCDiary.LastUserSeq;
+            LastDateTime = _TCDiary.LastDateTime;
+        }
     public int ChurchSeq { get; set; }
     public int DiarySeq     {get;set;}
     public DateTime InDate       {get;set;}
